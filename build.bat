a4657f8ea48806054a1575a641c3cc541334a8c7
zacharyGayford <zachary.gayford@gmail.com>
2026-06-01 19:19:24 -0500

    updated build script to use cmake

--- build.bat
+++ build.bat
@@ -1,11 +1,25 @@
 @echo off
+cls
+
+if "%1"=="clean" (
+	echo removing bin ...
+	rmdir /s /q bin
+)
+
+:: NOTE(zgayford): for now I am building with gcc & mingw on windows
+:: so that I can use pthreads
+cmake -S . -B bin -G Ninja ^
+	-DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc ^
+	-DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ ^
+	-DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+if errorlevel 1 exit /b
+
+del compile_commands.json
+move bin\compile_commands.json .
+
+cmake --build bin 
+if errorlevel 1 exit /b
 
 pushd bin
-clang++ ^
-	../src/*.cpp ^
-	-g -Wvarargs -Wextra -Wall -Werror -Wno-missing-braces ^
-	-I../src ^
-	-o main.exe
-main.exe
+tire_auralization.exe
 popd
-
